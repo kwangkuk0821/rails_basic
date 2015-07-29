b281de799f62d5597815e56b8242ec456f17d632
임광국 <kwangkuk0821@gmail.com>
2015-07-29 14:01:47 +0900

    컨트롤러에서 변수 선언 방법
    * 일반 변수는 '변수명 = 값' 형태로 사용하시면 됩니다.
    * view 에서 컨트롤러의 변수를 사용하고 싶다면 변수명의 앞에 @를 붙이면 됩니다.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    #일반 변수 선언할 경우
+    title = "안녕하세요."
+    content = "이번 choco 수업을 맡게된 임광국입니다."
+    #view 에서 변수를 사용하기 위해서
+    @title = "안녕하세요"
+    @content = "이번 choco 수업을 맡게된 임광국입니다."
   end
 end
